dd936ed0faedd487fa708176e39e0d631886c8d4
caozhengqian <1264419544@qq.com>
2024-06-30 01:09:59 +0800

    add  部署到阿里云docs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "MUI",
   description: "A VitePress Site",
+  base:"/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
