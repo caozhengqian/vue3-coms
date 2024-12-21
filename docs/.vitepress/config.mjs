9578a3e942d15bb942e6380679b56ddcbe9e6bfa
caozhengqian <1264419544@qq.com>
2024-12-21 19:41:08 +0800

    add  web

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "MUI",
   description: "A VitePress Site",
-  base:"/",
+  base:"/vue3-coms",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
